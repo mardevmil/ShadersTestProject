b0e18a34592abbbfc1763c3505521153da41c9aa
Marko Milovanovic <mardevmil@gmail.com>
2019-01-03 15:22:44 +0100

    AdvancedEffects

---
+++ Assets/MeshData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshData : MonoBehaviour
+{
+
+	void Start ()
+	{
+	    Mesh mesh = GetComponent<MeshFilter>().mesh;
+	    Vector3[] vertices = mesh.vertices;
+	    foreach (var v in vertices)
+	    {
+	        Debug.Log(v);
+	    }
+	}	
+}
